18feea740d4731ec46d06e0c82a945f6dfc23e67
donellaangio <donellaangio@gmail.com>
2026-05-26 21:20:35 -0300

    filtrado en tiempo y en frec

--- modulos/filtros.py
+++ modulos/filtros.py
@@ -1,6 +1,8 @@
 import numpy as np
 from graficos import graficar_t, graficar_f, graficar_analisis
 from respuestaf import respuesta_f
+from senales_temporales import generar_tono_puro
+
 
 def filtros_media_movil(M,N):
     """
@@ -48,13 +50,15 @@ def filtro_peine(b0, b1, b2, N):
     h : NumPy array
         Vector con la respuesta al impulso h[n].
     """
+
+    
     delta = np.zeros(N)
     delta[0] = 1
 
     filtro = np.array([b0,b1,b2])
     h = np.convolve(delta, filtro)[:N]
 
-    return h
+    return h 
 
 def filtro_fir(N, path="C:\\Users\\Administrator\\Documents\\untref\\DSP\\DSP\\archivos_tp1c2026\\fir_hamming_1000Hz.npy"):  
     #esto funciona solamente para mi compu pero bueno
@@ -94,8 +98,10 @@ def analisis_filtros(filtro, fs):
 
 
 hp = filtro_peine(1,0.5,1,N=512)  
-fcia, modulo, fase = analisis_filtros(hp,1)
+fcia, modulo, fase = analisis_filtros(hp,3000)
 x=graficar_analisis(fcia, modulo, fase)
+
+""""
 hp = filtro_peine(1,0,0,N=512)  
 fcia, modulo, fase = analisis_filtros(hp,1)
 x=graficar_analisis(fcia, modulo, fase)
@@ -111,3 +117,40 @@ fcia3, modulo3, fase3 = analisis_filtros(h3, 1)
 x1=graficar_analisis(fcia1, modulo1, fase1)
 x2=graficar_analisis(fcia2, modulo2, fase2)
 x3=graficar_analisis(fcia3, modulo3, fase3)
+"""
+
+
+def filtrar_temporal(filtro, señal):
+    """
+    agregar docstring
+    """
+    señal_filtrada = np.convolve(señal, filtro, mode='same')
+
+    return señal_filtrada
+
+"""
+tono_puro = generar_tono_puro(amplitud=0.5, duracion=1, fs=3000, frecuencia=1000)
+
+señal_filtrada = filtrar_temporal (hp, tono_puro) [:len(tono_puro)]  
+"""
+
+def filtrar_frecuencial(filtro, señal, fs):
+    """
+    agregar docstring
+    """
+    H_w = np.fft.rfft(filtro, n=len(señal))
+    X_w = np.fft.rfft(señal)
+    Y_w = H_w * X_w
+
+    señal_filtrada =np.fft.irfft(Y_w, n=len(señal))
+
+    return señal_filtrada
+
+"""
+señal_filtrada = filtrar_frecuencial (hp, tono_puro, fs=3000) 
+"""
+
+señal_filtrada_1 = filtrar_temporal(hp, tono_puro)
+
+graficar_f(fs=3000, señales=señal_filtrada_1, titulo="tono puro filtrado")
+
